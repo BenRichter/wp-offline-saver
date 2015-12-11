4dd4bf58c4aaac16c0b96d82ecd5b0274731fe4e
Ben <ben.k.richter@gmail.com>
2015-12-11 18:28:50 +0100

    Revert: Remove public frontside directory

--- admin/js/offline-saver-admin.js
+++ admin/js/offline-saver-admin.js
@@ -5,10 +5,6 @@
 	 * All of the code for your admin-facing JavaScript source
 	 * should reside in this file.
 	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
 	 * This enables you to define handlers, for when the DOM is ready:
 	 *
 	 * $(function() {
@@ -20,13 +16,12 @@
 	 * $( window ).load(function() {
 	 *
 	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
 	 */
+
+	$(function() {
+		console.log("Hallo Welt");
+
+
+	});
 
 })( jQuery );
